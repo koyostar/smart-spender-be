8ac53f1c558429e01c1a5ccf257fdf3d2d6f9001
aishu-ch <aishu.chinnaiya1112@gmail.com>
2024-04-30 20:27:37 +0800

    feat: signup/login

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const express = require('express')
 const path = require('path');
 const logger = require('morgan');
+
 require('dotenv').config();
 require('./config/database');
 
 const app = express()
-app.use(logger('dev'));
 
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(require('./config/checkToken'))
+
+app.use('/api/users', require('./routes/api/users'));
 
 app.get('/*', function(req, res) {
     res.send("Hello World");
   });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 4000;
 
 app.listen(port, function() {
   console.log(`Express app running on port ${port}`);

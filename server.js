7632cd15d0cc261bea35b140cd3597499f717843
aishu-ch <aishu.chinnaiya1112@gmail.com>
2024-05-01 09:55:27 +0800

    feat: add corsOption

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const path = require('path');
 const logger = require('morgan');
+const cors = require('cors')
 
 require('dotenv').config();
 require('./config/database');
 
 const app = express()
+let corsOption = {
+  origin: ['smart-spender.vercel.app'],
+}
+
+app.use(cors(corsOption))
 
 app.use(logger('dev'));
 app.use(express.json());

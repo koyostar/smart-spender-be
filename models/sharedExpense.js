1b2686b9314e614b1b982e7bf4f59e492d225748
Shermaine <shermaine.lsh@gmail.com>
2024-05-06 03:40:22 +0800

    updated sharedExpenses schema to ref expenseId to Expense

--- models/sharedExpense.js
+++ models/sharedExpense.js
@@ -1,29 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const sharedExpenseSchema = new Schema({
+const sharedExpenseSchema = new Schema(
+  {
     expenseId: {
-        type: Number,
+      type: Number,
+      ref: "Expense",
+      required: true,
     },
     user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     amountOwed: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
     amountPaid: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
     isPaid: {
-        type: Boolean,
-        default: false,
-    }
-}, {
-  toJSON: { virtuals: true }
-});
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+  }
+);
 
-module.exports = mongoose.model('SharedExpense', sharedExpenseSchema);
+module.exports = mongoose.model("SharedExpense", sharedExpenseSchema);

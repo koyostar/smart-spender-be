30d10ac6064a5a24dd4ac3ce1ef36d6ce4dd116e
Shermaine <shermaine.lsh@gmail.com>
2024-05-06 03:39:36 +0800

    completed statistics api

--- controllers/api/statistics.js
+++ controllers/api/statistics.js
@@ -6,6 +6,7 @@ async function calculateStats(req, res) {
   try {
     const { userid } = req.params;
     const user = await User.findById(userid);
+
     const totalExpenses = await Expense.aggregate([
       { $match: { createdBy: user._id } },
       {
@@ -16,6 +17,7 @@ async function calculateStats(req, res) {
         },
       },
     ]);
+
     const totalSharedExpenses = await SharedExpense.aggregate([
       { $match: { user: user._id } },
       {
@@ -25,7 +27,7 @@ async function calculateStats(req, res) {
         },
       },
     ]);
-    const unpaidSharedexpenses = await SharedExpense.aggregate([
+    const unpaidSharedExpenses = await SharedExpense.aggregate([
       { $match: { user: user._id, isPaid: false } },
       {
         $group: {
@@ -34,8 +36,35 @@ async function calculateStats(req, res) {
         },
       },
     ]);
-    return res.status(200).json({ expensesStats });
+
+    const userExpenses = await Expense.find({ createdBy: user._id });
+    const expenseIds = userExpenses.map((expense) => expense._id);
+    const sharedExpenseStats = await SharedExpense.aggregate([
+      {
+        $match: { expenseId: { $in: expenseIds }, isPaid: false },
+      },
+      {
+        $group: {
+          _id: null,
+          totalAmountIsOwed: { $sum: "$amountOwed" },
+        },
+      },
+    ]);
+
+    return res.status(200).json({
+      totalExpenses: totalExpenses[0] || {
+        totalExpenses: 0,
+        numofExpenses: 0,
+      },
+      totalSharedExpenses: totalSharedExpenses[0] || { totalOwed: 0 },
+      unpaidSharedExpenses: unpaidSharedExpenses[0] || { totalUnpaid: 0 },
+      sharedExpenseStats: sharedExpenseStats[0] || {
+        totalAmountIsOwed: 0,
+      },
+    });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
 }
+
+module.exports = { calculateStats };

a991d23e77460e406518fa5c641fe4060a9d7a33
leelingyan98 <58385937+leelingyan98@users.noreply.github.com>
2024-05-06 20:03:30 +0800

    feat: add friends to user model, add and remove friends

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,12 +3,24 @@ const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
 const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
+////////////////////////
+//        LOGIN
+////////////////////////
+
 // POST /api/users
 router.post('/', usersCtrl.create);
 // POST /api/users/login
 router.post('/login', usersCtrl.login);
 
 router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken)
+
+////////////////////////
+//     FUNCTIONAL
+////////////////////////
+
+router.post('/friends/add/:selecteduser', usersCtrl.friendsAdd)
+router.post('/friends/remove/:selecteduser', usersCtrl.friendsRemove)
+router.get('/friends', usersCtrl.friendsFindAll)
 
 router.get('/find', usersCtrl.findAll);
 router.get('/find/:username', usersCtrl.findByUsername)
